9f78ba74d7bde14efb5450a30b56c506e883fe53
naamanjagga <namanjagga@cedcommerce.com>
2022-02-19 10:28:55 +0530

    Output 1 if  is equal to , print 2 if  is greater than , otherwise output 3

--- src/handson.php
+++ src/handson.php
@@ -66,5 +66,18 @@ if($a == $b) {
   echo "No";
 }                //Output "Yes" if $a is equal to $b, otherwise output "No".
 
+echo "<br>";
+$a = 50;
+$b = 10;
+if ($a == $b) {
+  echo "1";
+} elseif
+ ($a > $b) {
+  echo "2";
+} else
+ {
+  echo "3";
+}                  //Output "1" if $a is equal to $b, print "2" if $a is greater than $b, otherwise output "3".
+
 
 ?>
