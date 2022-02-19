dab24a1b7a34c58ff7c0365bd7fe31bc4341a42e
naamanjagga <namanjagga@cedcommerce.com>
2022-02-19 10:03:37 +0530

    Create one variable named x, and one variable named y, then use the echo statement to output the sum of x and y.

--- src/handson.php
+++ src/handson.php
@@ -22,5 +22,10 @@ echo "Hello World";  //Statements in PHP have to end with a special character
 
 $txt = "Hello";  //Create a variable named txt and assign the value "Hello".
 
+echo "<br>";
+$x = 5;
+$y = 7;
+echo $x + $y;   //Create one variable named x, and one variable named y, then use the echo statement to output the sum of x and y.
+
 
 ?>
