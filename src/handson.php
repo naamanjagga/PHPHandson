40b83df87dffc26f0ff2b13e885ffb487465c28b
naamanjagga <namanjagga@cedcommerce.com>
2022-02-19 09:55:02 +0530

    Write the correct opening tag and close tag for PHP scripts.

--- src/handson.php
+++ src/handson.php
@@ -2,3 +2,9 @@
 
 echo "Hello World";  //Insert the missing part of the code below to output "Hello World".
 ?>
+
+
+<?php
+
+echo "This is PHP";   //Write the correct opening tag and close tag for PHP scripts.
+?>
